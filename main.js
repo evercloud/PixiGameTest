952fd895ca3c89e263eb42feed5a79a5a69e7323
scamporlino <claudio@MacBook-Pro-di-Claudio.local>
2022-09-29 23:11:03 +0200

    feat: added basic html and ts files

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const app = new PIXI.Application({ antialias: true });
+var app = new PIXI.Application({ antialias: true });
 document.body.appendChild(app.view);
-
-const graphics = new PIXI.Graphics();
-
+var graphics = new PIXI.Graphics();
 // Rectangle
 graphics.beginFill(0xDE3249);
 graphics.drawRect(50, 50, 100, 100);
 graphics.endFill();
-
 app.stage.addChild(graphics);
