7963ec4504c20e4895512ca3f6794f13ae760a71
scamporlino <claudio@MacBook-Pro-di-Claudio.local>
2022-09-30 16:36:55 +0200

    fix: minor changes, reverting from dead end

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// import * as PIXI from 'pixi.js';
 var app = new PIXI.Application({ width: 800, height: 800 });
 document.body.appendChild(app.view);
-var playerWidth = app.screen.width / 15;
-var playerHeight = app.screen.height / 15;
+var gridWidth = app.screen.width / 15;
+var gridHeight = app.screen.height / 15;
 //background
 var bg = new PIXI.Graphics();
 bg.beginFill(0x9ACB59);
@@ -11,28 +12,39 @@ app.stage.addChild(bg);
 //player
 var playerBody = new PIXI.Graphics();
 playerBody.beginFill(0x3F6FDE);
-playerBody.drawRect(0, 0, playerWidth, playerHeight);
+playerBody.drawRect(0, 0, gridWidth, gridHeight);
 playerBody.endFill();
-playerBody.position.x = (app.screen.width / 2) - (playerWidth / 2);
-playerBody.position.y = (app.screen.height / 2) - (playerHeight / 2);
+playerBody.position.x = (app.screen.width / 2) - (gridWidth / 2);
+playerBody.position.y = (app.screen.height / 2) - (gridHeight / 2);
 app.stage.addChild(playerBody);
 document.addEventListener('keydown', onKeyDown);
 function onKeyDown(key) {
-    console.log("PRESSED" + key.keyCode);
     // if W Key is pressed and not on the edge of level -> move
-    if (key.keyCode === 87 && playerBody.position.y > playerWidth / 2) {
-        playerBody.position.y -= playerHeight;
+    if (key.keyCode === 87 && playerBody.position.y > gridWidth / 2) {
+        playerBody.position.y -= gridHeight;
     }
     // S
-    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - playerHeight) {
-        playerBody.position.y += playerHeight;
+    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - gridHeight) {
+        playerBody.position.y += gridHeight;
     }
     // A
-    if (key.keyCode === 65 && playerBody.position.x > playerWidth / 2) {
-        playerBody.position.x -= playerWidth;
+    if (key.keyCode === 65 && playerBody.position.x > gridWidth / 2) {
+        playerBody.position.x -= gridWidth;
     }
     // D
-    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - playerWidth) {
-        playerBody.position.x += playerWidth;
+    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - gridWidth) {
+        playerBody.position.x += gridWidth;
     }
+}
+var timer = 0;
+var moveDuration = 1;
+app.ticker.add(function (delta) {
+    timer += delta;
+    if (timer > moveDuration) {
+        ManualUpdate();
+        timer = 0;
+    }
+});
+function ManualUpdate() {
+    console.log("UPDATING NOW. TIME: " + app.ticker);
 }
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
+// import * as PIXI from 'pixi.js';
+
 let app = new PIXI.Application({ width: 800, height: 800 });
 
 document.body.appendChild(app.view);
 
-var playerWidth: number = app.screen.width / 15;
-var playerHeight: number = app.screen.height / 15;
+var gridWidth: number = app.screen.width / 15;
+var gridHeight: number = app.screen.height / 15;
 
 //background
 const bg = new PIXI.Graphics();
@@ -15,34 +17,48 @@ app.stage.addChild(bg);
 //player
 const playerBody = new PIXI.Graphics();
 playerBody.beginFill(0x3F6FDE);
-playerBody.drawRect(0, 0, playerWidth, playerHeight);
+playerBody.drawRect(0, 0, gridWidth, gridHeight);
 playerBody.endFill();
-playerBody.position.x = (app.screen.width / 2) - (playerWidth / 2);
-playerBody.position.y = (app.screen.height / 2) - (playerHeight / 2);
+playerBody.position.x = (app.screen.width / 2) - (gridWidth / 2);
+playerBody.position.y = (app.screen.height / 2) - (gridHeight / 2);
 app.stage.addChild(playerBody);
 
 document.addEventListener('keydown', onKeyDown);
 
-function onKeyDown(key) {
-    console.log("PRESSED" + key.keyCode);
-
+function onKeyDown(key: KeyboardEvent) {
     // if W Key is pressed and not on the edge of level -> move
-    if (key.keyCode === 87 && playerBody.position.y > playerWidth / 2) {
-        playerBody.position.y -= playerHeight;
+    if (key.keyCode === 87 && playerBody.position.y > gridWidth / 2) {
+        playerBody.position.y -= gridHeight;
     }
 
     // S
-    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - playerHeight) {
-        playerBody.position.y += playerHeight;
+    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - gridHeight) {
+        playerBody.position.y += gridHeight;
     }
 
     // A
-    if (key.keyCode === 65 && playerBody.position.x > playerWidth / 2) {
-        playerBody.position.x -= playerWidth;
+    if (key.keyCode === 65 && playerBody.position.x > gridWidth / 2) {
+        playerBody.position.x -= gridWidth;
     }
 
     // D
-    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - playerWidth) {
-        playerBody.position.x += playerWidth;
+    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - gridWidth) {
+        playerBody.position.x += gridWidth;
     }
+}
+
+let timer = 0;
+let moveDuration = 1;
+
+app.ticker.add((delta) => {
+    timer += delta;
+
+    if (timer > moveDuration) {
+        ManualUpdate();
+        timer = 0;
+    }
+});
+
+function ManualUpdate() {
+    console.log("UPDATING NOW. TIME: " + app.ticker);
 }
