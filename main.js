f00fc66f1a2c8b78aa539567c7434f4b1bb1b7a2
scamporlino <claudio@MacBook-Pro-di-Claudio.local>
2022-09-30 21:51:57 +0200

    feat: added snake-ish movement

--- main.js
+++ main.js
@@ -1,50 +1,154 @@
 // import * as PIXI from 'pixi.js';
+// import { debug } from "console";
+//initialize app
 var app = new PIXI.Application({ width: 800, height: 800 });
 document.body.appendChild(app.view);
-var gridWidth = app.screen.width / 15;
-var gridHeight = app.screen.height / 15;
-//background
+//useful fields:
+var gridSpacing = 15;
+var gridWidth = app.screen.width / gridSpacing;
+var gridHeight = app.screen.height / gridSpacing;
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+//draw background
 var bg = new PIXI.Graphics();
 bg.beginFill(0x9ACB59);
 bg.drawRect(0, 0, app.screen.width, app.screen.height);
 bg.endFill();
 app.stage.addChild(bg);
-//player
+//create player
+var playerSpeed = 3;
 var playerBody = new PIXI.Graphics();
 playerBody.beginFill(0x3F6FDE);
-playerBody.drawRect(0, 0, gridWidth, gridHeight);
+playerBody.drawRect(-gridWidth / 2, -gridHeight / 2, gridWidth, gridHeight);
 playerBody.endFill();
-playerBody.position.x = (app.screen.width / 2) - (gridWidth / 2);
-playerBody.position.y = (app.screen.height / 2) - (gridHeight / 2);
+playerBody.position.x = (app.screen.width / 2);
+playerBody.position.y = (app.screen.height / 2);
 app.stage.addChild(playerBody);
+var currentDirection = Direction.Right;
+var nextDirection = Direction.Right;
+//add player keys listerner...
 document.addEventListener('keydown', onKeyDown);
+//...and handler
 function onKeyDown(key) {
-    // if W Key is pressed and not on the edge of level -> move
-    if (key.keyCode === 87 && playerBody.position.y > gridWidth / 2) {
-        playerBody.position.y -= gridHeight;
+    // if key is pressed, set next direction
+    if (key.keyCode === 87) {
+        SetNextDirection(Direction.Up);
+    } //W
+    if (key.keyCode === 83) {
+        SetNextDirection(Direction.Down);
+    } //S
+    if (key.keyCode === 65) {
+        SetNextDirection(Direction.Left);
+    } //A
+    if (key.keyCode === 68) {
+        SetNextDirection(Direction.Right);
+    } //D
+}
+//add timer text
+var timerText = app.stage.addChild(new PIXI.Text('SURVIVAL TIME: ', {
+    fontFamily: 'Arial',
+    fontSize: 24,
+    fill: 0xffffff,
+    align: 'center'
+}));
+timerText.anchor.set(0.5);
+timerText.x = app.screen.width / 2;
+timerText.y = 50;
+app.ticker.add(Update);
+function Update() {
+    UpdateTimerText();
+    //"SNAPPY" OLD VERSION
+    // timer += app.ticker.elapsedMS;
+    // if (timer <= moveDuration)
+    //     return;
+    // timer = 0;
+    UpdatePlayerPosition();
+    //update enemy positions
+    //check for collisions
+}
+function UpdateTimerText() {
+    timerText.text = 'SURVIVAL TIME: ' + Math.round(app.ticker.lastTime / 1000);
+}
+//PLAYER FUNCTIONS
+function UpdatePlayerPosition() {
+    //update with current direction
+    switch (currentDirection) {
+        case Direction.Up:
+            playerBody.position.y -= gridHeight * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Down:
+            playerBody.position.y += gridHeight * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Left:
+            playerBody.position.x -= gridWidth * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Right:
+            playerBody.position.x += gridWidth * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        default:
+            break;
     }
-    // S
-    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - gridHeight) {
-        playerBody.position.y += gridHeight;
-    }
-    // A
-    if (key.keyCode === 65 && playerBody.position.x > gridWidth / 2) {
-        playerBody.position.x -= gridWidth;
-    }
-    // D
-    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - gridWidth) {
-        playerBody.position.x += gridWidth;
+    if (currentDirection != nextDirection)
+        HandleChangeDirection();
+    if (IsOutOfBounds(playerBody.position.x, playerBody.position.y))
+        TriggerGameOver();
+}
+function IsOutOfBounds(x, y) {
+    if (x < 0 + gridWidth / 2 || x > app.screen.width - gridWidth / 2 ||
+        y < 0 + gridHeight / 2 || y > app.screen.width - gridHeight / 2)
+        return true;
+    return false;
+}
+function SetNextDirection(_direction) {
+    nextDirection = _direction;
+}
+var thresholdDistance = 1;
+function HandleChangeDirection() {
+    var distance;
+    var temp;
+    switch (currentDirection) {
+        case Direction.Up:
+        case Direction.Down:
+            temp = Math.round(playerBody.y / gridHeight);
+            distance = Math.abs(temp * gridHeight - playerBody.y);
+            // timerText.text = 'distance: ' + distance + " and grid point:" + temp;
+            if (distance >= thresholdDistance)
+                return;
+            playerBody.y = temp * gridHeight;
+            currentDirection = nextDirection;
+            break;
+        case Direction.Left:
+        case Direction.Right:
+            temp = Math.round(playerBody.x / gridWidth);
+            distance = Math.abs(temp * gridWidth - playerBody.x);
+            // timerText.text = 'distance: ' + distance + " and grid point:" + temp;
+            if (distance >= thresholdDistance)
+                return;
+            playerBody.x = temp * gridWidth;
+            currentDirection = nextDirection;
+            break;
+        default:
+            return;
     }
 }
-var timer = 0;
-var moveDuration = 1;
-app.ticker.add(function (delta) {
-    timer += delta;
-    if (timer > moveDuration) {
-        ManualUpdate();
-        timer = 0;
-    }
-});
-function ManualUpdate() {
-    console.log("UPDATING NOW. TIME: " + app.ticker);
+function TriggerGameOver() {
+    app.ticker.stop();
+    timerText.y = app.screen.height / 2;
+    timerText.text = "GAMEOVER! TIME: " + Math.round(app.ticker.lastTime / 1000);
 }
+/*
+//NOTES:
+//deltaTime counts how many frames were rendered in one update (30FPS => 2, 60FPS => 1)
+//to move the object 5 pixels per frame, use "5 * deltaTime"
+//to move "300 pixels per second", use "0.3 * elapsedMs"
+
+  
+//YOU CAN REMOVE LISTENERS FROM UPDATE, example:
+//   setTimeout (function(){
+//     app.ticker.remove(app.animationUpdate);
+//   },5000);*/ 
--- main.ts
+++ main.ts
@@ -1,64 +1,169 @@
 // import * as PIXI from 'pixi.js';
 
-let app = new PIXI.Application({ width: 800, height: 800 });
+// import { debug } from "console";
 
+//initialize app
+let app = new PIXI.Application({ width: 800, height: 800 });
 document.body.appendChild(app.view);
 
-var gridWidth: number = app.screen.width / 15;
-var gridHeight: number = app.screen.height / 15;
+//useful fields:
+var gridSpacing: number = 15;
+var gridWidth: number = app.screen.width / gridSpacing;
+var gridHeight: number = app.screen.height / gridSpacing;
 
-//background
+enum Direction { Up, Down, Left, Right }
+
+//draw background
 const bg = new PIXI.Graphics();
 bg.beginFill(0x9ACB59);
 bg.drawRect(0, 0, app.screen.width, app.screen.height);
 bg.endFill();
 app.stage.addChild(bg);
 
-//player
+//create player
+let playerSpeed: number = 3;
 const playerBody = new PIXI.Graphics();
 playerBody.beginFill(0x3F6FDE);
-playerBody.drawRect(0, 0, gridWidth, gridHeight);
+playerBody.drawRect(-gridWidth / 2, -gridHeight / 2, gridWidth, gridHeight);
 playerBody.endFill();
-playerBody.position.x = (app.screen.width / 2) - (gridWidth / 2);
-playerBody.position.y = (app.screen.height / 2) - (gridHeight / 2);
+playerBody.position.x = (app.screen.width / 2);
+playerBody.position.y = (app.screen.height / 2);
 app.stage.addChild(playerBody);
 
+let currentDirection = Direction.Right;
+let nextDirection = Direction.Right;
+
+//add player keys listerner...
 document.addEventListener('keydown', onKeyDown);
-
+//...and handler
 function onKeyDown(key: KeyboardEvent) {
-    // if W Key is pressed and not on the edge of level -> move
-    if (key.keyCode === 87 && playerBody.position.y > gridWidth / 2) {
-        playerBody.position.y -= gridHeight;
+    // if key is pressed, set next direction
+    if (key.keyCode === 87) { SetNextDirection(Direction.Up); } //W
+    if (key.keyCode === 83) { SetNextDirection(Direction.Down); } //S
+    if (key.keyCode === 65) { SetNextDirection(Direction.Left); } //A
+    if (key.keyCode === 68) { SetNextDirection(Direction.Right); } //D
+}
+
+//add timer text
+const timerText = app.stage.addChild(new PIXI.Text('SURVIVAL TIME: ', {
+    fontFamily: 'Arial',
+    fontSize: 24,
+    fill: 0xffffff,
+    align: 'center'
+}));
+
+timerText.anchor.set(0.5);
+timerText.x = app.screen.width / 2;
+timerText.y = 50;
+
+app.ticker.add(Update);
+
+function Update() {
+    UpdateTimerText();
+
+    //"SNAPPY" OLD VERSION
+    // timer += app.ticker.elapsedMS;
+    // if (timer <= moveDuration)
+    //     return;
+    // timer = 0;
+
+    UpdatePlayerPosition();
+
+    //update enemy positions
+    //check for collisions
+}
+
+function UpdateTimerText() {
+    timerText.text = 'SURVIVAL TIME: ' + Math.round(app.ticker.lastTime / 1000);
+}
+
+//PLAYER FUNCTIONS
+function UpdatePlayerPosition() {
+    //update with current direction
+    switch (currentDirection) {
+        case Direction.Up:
+            playerBody.position.y -= gridHeight * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Down:
+            playerBody.position.y += gridHeight * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Left:
+            playerBody.position.x -= gridWidth * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        case Direction.Right:
+            playerBody.position.x += gridWidth * playerSpeed * app.ticker.elapsedMS / 1000;
+            break;
+        default:
+            break;
     }
 
-    // S
-    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - gridHeight) {
-        playerBody.position.y += gridHeight;
-    }
+    if (currentDirection != nextDirection)
+        HandleChangeDirection();
 
-    // A
-    if (key.keyCode === 65 && playerBody.position.x > gridWidth / 2) {
-        playerBody.position.x -= gridWidth;
-    }
+    if (IsOutOfBounds(playerBody.position.x, playerBody.position.y))
+        TriggerGameOver();
+}
 
-    // D
-    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - gridWidth) {
-        playerBody.position.x += gridWidth;
+
+function IsOutOfBounds(x: number, y: number): boolean {
+    if (x < 0 + gridWidth / 2 || x > app.screen.width - gridWidth / 2 ||
+        y < 0 + gridHeight / 2 || y > app.screen.width - gridHeight / 2)
+        return true;
+    return false;
+}
+
+function SetNextDirection(_direction: Direction) {
+    nextDirection = _direction;
+}
+
+let thresholdDistance: number = 1;
+function HandleChangeDirection() {
+    let distance: number;
+    let temp: number;
+
+    switch (currentDirection) {
+        case Direction.Up:
+        case Direction.Down:
+            temp = Math.round(playerBody.y / gridHeight);
+            distance = Math.abs(temp * gridHeight - playerBody.y);
+            // timerText.text = 'distance: ' + distance + " and grid point:" + temp;
+
+            if (distance >= thresholdDistance)
+                return;
+
+            playerBody.y = temp * gridHeight;
+            currentDirection = nextDirection;
+            break;
+        case Direction.Left:
+        case Direction.Right:
+            temp = Math.round(playerBody.x / gridWidth);
+            distance = Math.abs(temp * gridWidth - playerBody.x);
+            // timerText.text = 'distance: ' + distance + " and grid point:" + temp;
+
+            if (distance >= thresholdDistance)
+                return;
+
+            playerBody.x = temp * gridWidth;
+            currentDirection = nextDirection;
+            break;
+        default:
+            return;
     }
 }
 
-let timer = 0;
-let moveDuration = 1;
-
-app.ticker.add((delta) => {
-    timer += delta;
-
-    if (timer > moveDuration) {
-        ManualUpdate();
-        timer = 0;
-    }
-});
-
-function ManualUpdate() {
-    console.log("UPDATING NOW. TIME: " + app.ticker);
+function TriggerGameOver() {
+    app.ticker.stop();
+    timerText.y = app.screen.height / 2;
+    timerText.text = "GAMEOVER! TIME: " + Math.round(app.ticker.lastTime / 1000);
 }
+/*
+//NOTES:
+//deltaTime counts how many frames were rendered in one update (30FPS => 2, 60FPS => 1)
+//to move the object 5 pixels per frame, use "5 * deltaTime"
+//to move "300 pixels per second", use "0.3 * elapsedMs"
+
+  
+//YOU CAN REMOVE LISTENERS FROM UPDATE, example:
+//   setTimeout (function(){
+//     app.ticker.remove(app.animationUpdate);
+//   },5000);*/
