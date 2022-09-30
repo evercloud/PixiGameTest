440099d8050d1044c3d395f0be284912695d3522
scamporlino <claudio@MacBook-Pro-di-Claudio.local>
2022-09-30 12:35:33 +0200

    feat: added bg and player tests

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-var app = new PIXI.Application({ antialias: true });
+var app = new PIXI.Application({ width: 800, height: 800 });
 document.body.appendChild(app.view);
-var graphics = new PIXI.Graphics();
-// Rectangle
-graphics.beginFill(0xDE3249);
-graphics.drawRect(50, 50, 100, 100);
-graphics.endFill();
-app.stage.addChild(graphics);
+var playerWidth = app.screen.width / 15;
+var playerHeight = app.screen.height / 15;
+//background
+var bg = new PIXI.Graphics();
+bg.beginFill(0x9ACB59);
+bg.drawRect(0, 0, app.screen.width, app.screen.height);
+bg.endFill();
+app.stage.addChild(bg);
+//player
+var playerBody = new PIXI.Graphics();
+playerBody.beginFill(0x3F6FDE);
+playerBody.drawRect(0, 0, playerWidth, playerHeight);
+playerBody.endFill();
+playerBody.position.x = (app.screen.width / 2) - (playerWidth / 2);
+playerBody.position.y = (app.screen.height / 2) - (playerHeight / 2);
+app.stage.addChild(playerBody);
+document.addEventListener('keydown', onKeyDown);
+function onKeyDown(key) {
+    console.log("PRESSED" + key.keyCode);
+    // if W Key is pressed and not on the edge of level -> move
+    if (key.keyCode === 87 && playerBody.position.y > playerWidth / 2) {
+        playerBody.position.y -= playerHeight;
+    }
+    // S
+    if (key.keyCode === 83 && playerBody.position.y < app.screen.height - playerHeight) {
+        playerBody.position.y += playerHeight;
+    }
+    // A
+    if (key.keyCode === 65 && playerBody.position.x > playerWidth / 2) {
+        playerBody.position.x -= playerWidth;
+    }
+    // D
+    if (key.keyCode === 68 && playerBody.position.x < app.screen.width - playerWidth) {
+        playerBody.position.x += playerWidth;
+    }
+}
